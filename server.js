c3700625aebf4d6e9e2782737c01cfff28efb97d
Omar Nashawati <oceanprime.on@gmail.com>
2026-02-21 18:46:24 +0300

    conect to mongo db

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 import app from './app.js';
+import connectDB from './db/connect.js';
 
 const PORT = process.env.PORT || 3100;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
