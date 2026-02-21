65b6fece752897879d6b990bb95d46f2130eed5c
Omar Nashawati <oceanprime.on@gmail.com>
2026-02-21 17:31:33 +0300

    create auth and task controllers

---
+++ controllers/taskController.js
@@ -0,0 +1,15 @@
+export const createTask = async (req, res) => {
+  res.send('createTask');
+};
+
+export const getTasks = async (req, res) => {
+  res.send('getTasks');
+};
+
+export const updateTask = async (req, res) => {
+  res.send('updateTask');
+};
+
+export const deleteTask = async (req, res) => {
+  res.send('deleteTask');
+};
