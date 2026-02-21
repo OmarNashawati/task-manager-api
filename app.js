d73dcac6d2dfa01a8f87755f8c4030bfae6ca8ab
Omar Nashawati <oceanprime.on@gmail.com>
2026-02-21 17:38:33 +0300

    create auth and task routers

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import morgan from 'morgan';
 
 import { notFound } from './middleware/not-found.js';
 
+import authRoute from './routes/authRoute.js';
+import taskRoute from './routes/taskRoute.js';
+
 const app = express();
 
 app.use(express.json());
@@ -12,6 +15,9 @@ app.use(morgan('tiny'));
 app.get('/', (req, res) => {
   res.send('Task manager api');
 });
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/tasks', taskRoute);
 
 app.use(notFound);
 
